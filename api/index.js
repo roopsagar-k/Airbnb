df45bc1f2db9c82e457aa35c40cd60c8682c02b6
roopsagar-k <roopsagarudayar9854@gmail.com>
2025-02-21 18:41:29 +0530

    update

--- api/index.js
+++ api/index.js
@@ -40,9 +40,9 @@ function authenticateToken(req, res, next) {
   }
   jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, {}, (err, user) => {
     if (err) {
-      return res.status(403).json({
-        message: "Invalid or expired token",
-        error: err.message, // Include error details
+      return res.status(403).json({ 
+        message: "Invalid or expired token", 
+        error: err.message // Include error details
       });
     }
     req.user = user;
@@ -117,7 +117,7 @@ app.post("/login", async (req, res) => {
         res
           .cookie("token", token, {
             httpOnly: true,
-            secure: true,
+            secure: true, 
             sameSite: "none",
           })
           .json({ message: "Login successful", user: user.rows[0] });
@@ -510,6 +510,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port,"0.0.0.0", () => {
   console.log(`App is running at the port ${port}`);
 });
